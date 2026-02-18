d3d4528c09e48909b74334f009b5c32aa91b8648
Faketyfake <166341852+Faketyfake@users.noreply.github.com>
2026-02-18 16:37:48 -0500

    Added saved recipes feature

--- main.py
+++ main.py
@@ -13,10 +13,34 @@ def main():
 
         choice = input("\nPlease enter your choice (1-7): ")
 
-        if choice == '7':
-            print("Thank you for using the Grocery Main Planner. Goodbye!")
+        if choice == "1":
+            print("Coming soon!")
+        elif choice == "3":
+            view_recipes()
+        elif choice == "4":
+            add_recipe()
+        elif choice == "7":
+            print("Goodbye!")
             break
-        else: 
-            print("This feature is currently under development. Please check back later.")
+        else:
+            print("Option coming soon!")    
+
+saved_recipes = {}
+def add_recipe():
+    name = input("Enter the name of the recipe: ")
+    ingredients = input("Enter the ingredients (comma separated): ")
+    ingredients_list = [i.strip() for i in ingredients.split(',')]
+    saved_recipes[name] = ingredients_list
+    print(f"\n'{name}' added successfully!")
+
+def view_recipes():
+    if not saved_recipes:
+        print("\nNo recipes saved yet.")
+    else:
+        print("\n=== Saved Recipes ===")
+        for meal, ingredients in saved_recipes.items():
+            print(f"\n{meal}: ")
+            for ingredient in ingredients:
+                print(f" - {ingredient}")
 
 main()
